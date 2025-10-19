b29bd3f6067034c765f1656a25f2e770d15cf210
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 12:17:58 +0330

    #34343 feat(something): sorksdfa dflkajsdl

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-export default { extends: ['@commitlint/config-conventional'] };
+// export default { extends: ['@commitlint/config-conventional'] };
+// commitlint.config.js
+const config = {
+	rules: {
+		'header-pattern': [2, 'always', /^.*#\d+.*$/],
+		'header-pattern-message': [
+			2,
+			'always',
+			'Commit must contain an issue number like #123 or #000349',
+		],
+		'header-max-length': [2, 'always', 200],
+	},
+};
+
+module.exports = config;
