c2cff57a4c28cc99a0ace9968a0461e63d4f3fd3
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 09:11:51 +0330

    #56059 chore(huskry)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,38 @@
-const commitRulesPlugin = {
-	rules: {
-		'header-match-pattern': ({ header }, _when, regex) => {
-			const pattern = new RegExp(regex);
-			const valid = pattern.test(header);
-			return [valid];
+export default {
+	parserPreset: {
+		parserOpts: {
+			headerPattern:
+				/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
 		},
+	},
+	rules: {
+		'header-match-pattern': [2, 'always'],
 	},
 };
 
-export default {
-	plugins: [commitRulesPlugin],
-	rules: {
-		'header-match-pattern': [
-			2,
-			'always',
-			/^#\\d{1,10}\\s+(feat|fix|docs|refactor|ci|chore|style|test)(\\([a-zA-Z0-9_-]+\\))?:\\s.{1,200}$/,
-		],
-	},
-	// // Must start with # and a number, then space
-	// 'header-match-pattern': [
-	// 	2,
-	// 	'always',
-	// 	/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
-	// ],
-};
+// const commitRulesPlugin = {
+// 	rules: {
+// 		'header-match-pattern': ({ header }, _when, regex) => {
+// 			const pattern = new RegExp(regex);
+// 			const valid = pattern.test(header);
+// 			return [valid];
+// 		},
+// 	},
+// };
+
+// export default {
+// 	plugins: [commitRulesPlugin],
+// 	rules: {
+// 		'header-match-pattern': [
+// 			2,
+// 			'always',
+// 			/^#\\d{1,10}\\s+(feat|fix|docs|refactor|ci|chore|style|test)(\\([a-zA-Z0-9_-]+\\))?:\\s.{1,200}$/,
+// 		],
+// 	},
+// 	// // Must start with # and a number, then space
+// 	// 'header-match-pattern': [
+// 	// 	2,
+// 	// 	'always',
+// 	// 	/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
+// 	// ],
+// };
