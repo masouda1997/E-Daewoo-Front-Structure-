a1b4d8eaee8221d16e875f5023b42b456bc334dd
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 14:17:11 +0330

    #56059 chore: husky,linter, edditorConfig , prettier added

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,22 +4,10 @@ const config = {
 		'header-pattern': [
 			2,
 			'always',
-			/^(?<ticket>#\d+)\s+(?<type>feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(?<scope>\(\w+\))?:\s+(?<subject>.+)$/,
+			/^#\d+\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s[a-zA-Z0-9].{0,170}$/,
 		],
 		'header-max-length': [2, 'always', 200],
 	},
 };
-
-module.exports = config;
-// const config = {
-// 	rules: {
-// 		'header-pattern': [
-// 			2,
-// 			'always',
-// 			/^#\d+\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s[a-zA-Z0-9].{0,178}$/,
-// 		],
-// 		'header-max-length': [2, 'always', 200],
-// 	},
-// };
 
 module.exports = config;
