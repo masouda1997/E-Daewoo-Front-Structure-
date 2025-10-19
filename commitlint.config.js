6ceaa214d428711269220e4edc68d72cbd7d7419
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 09:58:59 +0330

    #56059 chore(huskry)

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ export default {
 		'header-match-pattern': [
 			2,
 			'always',
-			'/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/',
+			/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
 		],
 	},
 	plugins: [
