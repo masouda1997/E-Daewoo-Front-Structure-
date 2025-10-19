df783b9381f191b70cd55661d7729b364ef013dd
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 09:09:58 +0330

    #56059 chore(huskry)

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,7 +14,7 @@ export default {
 		'header-match-pattern': [
 			2,
 			'always',
-			'^#\\d{1,10}\\s+(feat|fix|docs|refactor|ci|chore|style|test)(\\([a-zA-Z0-9_-]+\\))?:\\s.{1,200}$',
+			/^#\\d{1,10}\\s+(feat|fix|docs|refactor|ci|chore|style|test)(\\([a-zA-Z0-9_-]+\\))?:\\s.{1,200}$/,
 		],
 	},
 	// // Must start with # and a number, then space
