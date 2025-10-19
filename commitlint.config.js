44d9316b459137145a70e39d5b8048555d9102ce
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 09:04:26 +0330

    #56059 chore(huskry)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,26 @@
-export default {
+const commitRulesPlugin = {
 	rules: {
-		// Must start with # and a number, then space
+		'header-match-pattern': ({ header }, _when, regex) => {
+			const pattern = new RegExp(regex);
+			const valid = pattern.test(header);
+			return [valid];
+		},
+	},
+};
+
+export default {
+	plugins: [commitRulesPlugin],
+	rules: {
 		'header-match-pattern': [
 			2,
 			'always',
-			/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
+			'^#\\d{1,10}\\s+(feat|fix|docs|refactor|ci|chore|style|test)(\\([a-zA-Z0-9_-]+\\))?:\\s.{1,200}$',
 		],
 	},
-	plugins: [
-		{
-			rules: {
-				'header-match-pattern': ({ header }, when = 'always', regex) => {
-					const pattern = new RegExp(regex);
-					const valid = pattern.test(header);
-					return [valid];
-				},
-			},
-		},
-	],
+	// // Must start with # and a number, then space
+	// 'header-match-pattern': [
+	// 	2,
+	// 	'always',
+	// 	/^#\d{1,10}\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s.{1,200}$/,
+	// ],
 };
