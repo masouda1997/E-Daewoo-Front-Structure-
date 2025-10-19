7c371eefadec31369cd3d7d0235883bbd59261aa
Masoud anaraki <masoud30376@gmail.com>
2025-10-19 12:46:16 +0330

    #48574 : sorksdfa dflkajsdl

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,10 +4,22 @@ const config = {
 		'header-pattern': [
 			2,
 			'always',
-			/^#\d+\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s[a-zA-Z0-9].{0,178}$/,
+			/^(?<ticket>#\d+)\s+(?<type>feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(?<scope>\(\w+\))?:\s+(?<subject>.+)$/,
 		],
 		'header-max-length': [2, 'always', 200],
 	},
 };
+
+module.exports = config;
+// const config = {
+// 	rules: {
+// 		'header-pattern': [
+// 			2,
+// 			'always',
+// 			/^#\d+\s+(feat|fix|docs|refactor|ci|chore|style|test)(\([a-zA-Z0-9_-]+\))?:\s[a-zA-Z0-9].{0,178}$/,
+// 		],
+// 		'header-max-length': [2, 'always', 200],
+// 	},
+// };
 
 module.exports = config;
