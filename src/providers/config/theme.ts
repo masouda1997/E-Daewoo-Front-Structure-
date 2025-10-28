c7b8f97d8386e15ebf3ed1040876098f6bfa2718
Masoud anaraki <masoud30376@gmail.com>
2025-10-28 12:49:23 +0330

    #0 fix(pre commit): uncomment add all file to stage

--- src/providers/config/theme.ts
+++ src/providers/config/theme.ts
@@ -1,20 +1,20 @@
-import type { ThemeConfig } from "antd"
+import type { ThemeConfig } from 'antd';
 
 const theme: ThemeConfig = {
-  token: {
-    fontSize: 16,
-    colorPrimary: "#3a87bf",
-    fontFamily: "inherit",
-    colorBgLayout: "transparent",
-  },
+	token: {
+		fontSize: 16,
+		colorPrimary: '#3a87bf',
+		fontFamily: 'inherit',
+		colorBgLayout: 'transparent',
+	},
 
-  components: {
-    Menu: {
-      subMenuItemBg: "white",
-      itemBorderRadius: 2,
-      itemHoverBg: "#f9fbfc",
-    },
-  },
-}
+	components: {
+		Menu: {
+			subMenuItemBg: 'white',
+			itemBorderRadius: 2,
+			itemHoverBg: '#f9fbfc',
+		},
+	},
+};
 
-export default theme
+export default theme;
